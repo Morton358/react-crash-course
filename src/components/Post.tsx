193e81535b75d0d3ceb24f37cb86e9c4ad50bb42
Wlodek <vovankovalchuk358@gmail.com>
2026-01-15 17:28:31 +0100

    show Post component with random name (out of 10 predifined names)

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,7 +1,10 @@
+const names = ['Liam', 'Olivia', 'Noah', 'Emma', 'Oliver', 'Ava', 'Elijah', 'Sophia', 'James', 'Isabella'];
+
 function Post() {
+  const randomName = names[Math.floor(Math.random() * names.length)];
   return (
     <div>
-      <p>Wlodek</p>
+      <p>{randomName}</p>
       <p>React.js is awesome</p>
     </div>
   );
